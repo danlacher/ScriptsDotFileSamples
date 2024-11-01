6d39cf875a186544c182aab1ff75abf6edf0bf80
Dan Lacher <12863475+danlacher@users.noreply.github.com>
2024-11-01 11:46:14 -0400

    Create pixels-threaded.py
    A threaded version of pixel creation script, not currently working in the threading the correct files are not being created, may need to allocate a pixel per thread.  Special thanks to @beneeb304 for assisting with how to quickly do the threading

---
+++ Misc/pixels-threaded.py
@@ -0,0 +1,41 @@
+from PIL import Image # Pillow Library https://python-pillow.org/
+import threading # import threading library
+
+img = Image.new(mode = "RGB", size = (1,1))
+pixels = img.load() # Creates the pixel map
+
+def thread_process(start, end):
+    for x in range(start, end):
+        for y in range(start, end):
+            for z in range (start, end):
+                pixels[0,0] = (x,y,z)
+                name = str(x).rjust(3,'0')+'_'+str(y).rjust(3,'0')+'_'+str(z).rjust(3,'0')+'.gif'
+                img.save(format='GIF', fp=name)
+
+t0 = threading.Thread(target=thread_process, args=(0, 32))
+t1 = threading.Thread(target=thread_process, args=(32, 64))
+t2 = threading.Thread(target=thread_process, args=(64, 96))
+t3 = threading.Thread(target=thread_process, args=(96, 128))
+t4 = threading.Thread(target=thread_process, args=(128, 160))
+t5 = threading.Thread(target=thread_process, args=(160, 192))
+t6 = threading.Thread(target=thread_process, args=(192, 224))
+t7 = threading.Thread(target=thread_process, args=(224, 256))
+
+t0.start()
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+t6.start()
+t7.start()
+
+t0.join()
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+t5.join()
+t6.join()
+t7.join()
