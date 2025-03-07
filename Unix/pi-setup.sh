bbef1464325e4b3df3e2b9b4366d6222dec6da06
Dan Lacher <12863475+danlacher@users.noreply.github.com>
2025-03-07 10:40:46 -0500

    update
    Pi-Hole updates

--- Unix/pi-setup.sh
+++ Unix/pi-setup.sh
@@ -2,6 +2,15 @@
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install at screen rpi-connect nmon stress
+sudo apt -y install at screen rpi-connect nmon stress pigz git
+sudo apt -y autoremove
 
 ssh-keygen -b 4096
+
+rpi-connect on
+rpi-connect signin
+rpi-connect status
+
+mkdir GitHub
+cd GitHub
+git clone https://github.com/danlacher/ScriptsDotFileSamples
